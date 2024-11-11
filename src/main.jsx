7d6997099fde36416faa051f35d0e0a99d2206fd
PTParra <49121638+PTParra@users.noreply.github.com>
2024-11-11 17:16:39 +0100

    layout component hecho con estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import { LayoutComponent } from './components/layoutComponent.jsx'
+import './main.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LayoutComponent />}>
+          <Route path='' element={<h1>BIENVENIDO</h1>} />
+          <Route path='buscar' element={<h1>¿Que quieres buscar?</h1>} />
+          <Route path='busqueda/:input' element={<h1>Resultados de:</h1>} />
+          <Route path='mis-fotos' element={<h1>Mis Fotos</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 )
